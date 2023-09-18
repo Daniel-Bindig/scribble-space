0a560085cca5268861b9d5346469f376ba0dafb2
Alex <github@alexbi.shop>
2023-09-18 21:48:19 +0900

    share model

---
+++ models/share.js
@@ -0,0 +1,52 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection.js');
+const Entry = require('./entry.js');
+
+class Share extends Model {}
+
+Share.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  entryId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Entry,
+      key: 'id'
+    },
+    allowNull: false
+  },
+  sharedUserId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    },
+    allowNull: false
+  },
+  accessKey: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    onUpdate: DataTypes.NOW
+  }
+}, {
+  sequelize,
+  modelName: 'Share',
+  tableName: 'SharedEntries',
+  timestamps: true
+});
+
+Share.belongsTo(Entry, { foreignKey: 'entryId' });
+Share.belongsTo(User, { foreignKey: 'sharedUserId' });
+
+module.exports = Share;
