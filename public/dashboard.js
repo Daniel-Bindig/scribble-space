2c0013bdebc6305c15a0cdd749be2263df96ffba
Alex <github@alexbi.shop>
2023-09-22 18:57:05 +0900

    Cleanup and added comments for direction

--- public/dashboard.js
+++ public/dashboard.js
@@ -43,3 +43,13 @@ menuItems.forEach(item => {
         }
     });
 });
+
+// TODO: Add "Create new event" button to left panel
+// When clicked, open modal with form to create new event
+
+// Sequence of events:
+// On page load: pull down preview events from server and populate the calendar days with them, including the event IDs
+// When a day is clicked: pull down the events for that day and populate the left side panel with them
+// Note that user can click on a day with no entries, and nothing should occur
+// When an entry in the left panel is clicked, open a modal for viewing/editing the event. Including a share button
+// Share function to be implemented by Alex
