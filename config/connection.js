41c9bf475d9ea0398591fb6795186288b04c4ac8
Alex <github@alexbi.shop>
2023-09-25 22:22:56 +0900

    Debug messages

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log("===================== USING JAWSDB =====================")
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log("===================== USING LOCAL DB =====================")
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
